155466792d4c8a4c87de5349529e68ab0cfd2d93
yk4716 <yk4716@bs.amalnet.k12.il>
2025-04-24 18:35:08 +0300

    Design phase 2

--- app/src/main/java/com/example/ex0611/MainActivity.java
+++ app/src/main/java/com/example/ex0611/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex0611;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch swDN;
+    Button btn;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swDN = (Switch) findViewById(R.id.swDN);
+        btn = (Button) findViewById(R.id.btn);
+
     }
 }
